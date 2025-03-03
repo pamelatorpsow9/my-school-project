c083ffd7c5bca2424735ee110473628071af9e6d
pamelatorpsow9 <pamelatorpsow9@gmx.com>
2025-03-03 13:46:37 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,15 @@
+ <?php
+
+$student_name = "John Doe";
+$student_age = 20;
+$student_address = "123 Main St.";
+$student_city = "Anytown";
+$student_state = "CA";
+$student_zip = 98765;
+
+echo "<h1>Student Information</h1>";
+echo "<p><strong>Name: </strong>" . $student_name . "</p>";
+echo "<p><strong>Age: </strong>" . $student_age . "</p>";
+echo "<p><strong>Address: </strong>" . $student_address . ", " . $student_city . ", " . $student_state . " " . $student_zip . "</p>";
+
+?>
