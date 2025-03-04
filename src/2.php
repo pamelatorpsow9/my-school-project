1330f82c1d5de217c34cbea62314fadda4616ba0
pamelatorpsow9 <pamelatorpsow9@gmx.com>
2025-03-04 14:27:48 +0000

    Add 2.php

---
+++ src/2.php
@@ -0,0 +1,10 @@
+<?php
+// Your code here
+$name = $_POST['name'];
+$age = $_POST['age'];
+$country = $_POST['country'];
+
+echo "Hello $name!";
+echo "You are $age years old.";
+echo "Your country of origin is $country.";
+?>
